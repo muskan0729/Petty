84cb55d4841fb3d165fee81971febf356d51fac8
muskan0729 <muskan@spay.live>
2026-03-18 17:17:05 +0530

    images uplaoding

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -62,6 +62,54 @@ const Footer = () => {
           <p className="text-sm text-[#0B2F5B] mt-2">
             Email: askme@universalconnect.com
           </p>
+{/* Social Icons */}
+<div className="flex gap-4 mt-4">
+
+  {/* Facebook */}
+  <a href="#" className="bg-white p-2 rounded-full hover:bg-[#0B2F5B] group transition">
+    <svg
+      className="w-5 h-5 text-[#0B2F5B] group-hover:text-white"
+      fill="currentColor"
+      viewBox="0 0 24 24"
+    >
+      <path d="M22 12a10 10 0 10-11.63 9.87v-6.99H7.9V12h2.47V9.8c0-2.44 1.45-3.8 3.68-3.8 1.07 0 2.2.19 2.2.19v2.42h-1.24c-1.22 0-1.6.76-1.6 1.54V12h2.72l-.43 2.88h-2.29v6.99A10 10 0 0022 12z" />
+    </svg>
+  </a>
+
+  {/* Instagram */}
+  <a href="#" className="bg-white p-2 rounded-full hover:bg-[#0B2F5B] group transition">
+    <svg
+      className="w-5 h-5 text-[#0B2F5B] group-hover:text-white"
+      fill="currentColor"
+      viewBox="0 0 24 24"
+    >
+      <path d="M7 2C4.24 2 2 4.24 2 7v10c0 2.76 2.24 5 5 5h10c2.76 0 5-2.24 5-5V7c0-2.76-2.24-5-5-5H7zm10 2a3 3 0 013 3v10a3 3 0 01-3 3H7a3 3 0 01-3-3V7a3 3 0 013-3h10zm-5 3.8A5.2 5.2 0 106 13a5.2 5.2 0 006-5.2zm0 2A3.2 3.2 0 118.8 13 3.2 3.2 0 0112 9.8zm4.8-.9a1.2 1.2 0 11-1.2-1.2 1.2 1.2 0 011.2 1.2z" />
+    </svg>
+  </a>
+
+  {/* LinkedIn */}
+  <a href="#" className="bg-white p-2 rounded-full hover:bg-[#0B2F5B] group transition">
+    <svg
+      className="w-5 h-5 text-[#0B2F5B] group-hover:text-white"
+      fill="currentColor"
+      viewBox="0 0 24 24"
+    >
+      <path d="M20.45 20.45h-3.55v-5.6c0-1.33-.03-3.05-1.86-3.05-1.86 0-2.14 1.45-2.14 2.95v5.7H9.35V9h3.4v1.56h.05c.47-.9 1.63-1.86 3.35-1.86 3.58 0 4.24 2.36 4.24 5.42v6.33zM5.34 7.43a2.06 2.06 0 110-4.12 2.06 2.06 0 010 4.12zM7.12 20.45H3.56V9h3.56v11.45z" />
+    </svg>
+  </a>
+
+  {/* Twitter */}
+  <a href="#" className="bg-white p-2 rounded-full hover:bg-[#0B2F5B] group transition">
+    <svg
+      className="w-5 h-5 text-[#0B2F5B] group-hover:text-white"
+      fill="currentColor"
+      viewBox="0 0 24 24"
+    >
+      <path d="M22.46 6c-.77.35-1.6.58-2.46.69a4.3 4.3 0 001.88-2.37 8.59 8.59 0 01-2.73 1.04A4.28 4.28 0 0016.07 4c-2.37 0-4.3 1.92-4.3 4.3 0 .34.04.67.11.99A12.14 12.14 0 013 5.16a4.3 4.3 0 001.33 5.74 4.23 4.23 0 01-1.95-.54v.05c0 2.08 1.48 3.82 3.44 4.21a4.28 4.28 0 01-1.94.07 4.3 4.3 0 004.01 2.98A8.6 8.6 0 012 19.54 12.13 12.13 0 008.56 21c7.88 0 12.2-6.53 12.2-12.2l-.01-.56A8.7 8.7 0 0022.46 6z" />
+    </svg>
+  </a>
+
+</div>
 
 
         </div>
--- src/components/Home/Section3.jsx
+++ src/components/Home/Section3.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import collar from "../../images/cate_1.png";
-import leash from "../../images/cate_1.png";
-import harness from "../../images/cate_1.png";
-import accessory from "../../images/cate_1.png";
+import collar from "../../images/dog_collar.png";
+import leash from "../../images/leashes.png";
+import harness from "../../images/harness.png";
+import accessory from "../../images/pet_access.png";
 
 const Section3 = () => {
   return (
@@ -31,10 +31,10 @@ const Section3 = () => {
         {/* Product 1 */}
         <div className="bg-white rounded-2xl shadow hover:shadow-xl transition overflow-hidden" data-aos="fade-up">
           <img
-          //  src={collar}
-          src="https://img.freepik.com/free-vector/realistic-vector-brown-leather-dog-collar-pets-isolated-gradient-background_1284-48381.jpg?t=st=1773588117~exp=1773591717~hmac=b094818b2dfd40d07cc6326b7bebb5198e9353b00294a4ae7f92364dfa5cb8a3&w=1480"
+           src={collar}
+          // src="https://img.freepik.com/free-vector/realistic-vector-brown-leather-dog-collar-pets-isolated-gradient-background_1284-48381.jpg?t=st=1773588117~exp=1773591717~hmac=b094818b2dfd40d07cc6326b7bebb5198e9353b00294a4ae7f92364dfa5cb8a3&w=1480"
 
-           className="h-60 w-full object-cover" alt="colloar"/>
+           className="h-60 w-full object-contain bg-[#ebd0b6]" alt="colloar"/>
           <div className="p-5 text-center">
             <h3 className="font-semibold text-lg text-[#0B2F5B]">
               Dog Collars
@@ -45,8 +45,8 @@ const Section3 = () => {
         {/* Product 2 */}
         <div className="bg-white rounded-2xl shadow hover:shadow-xl transition overflow-hidden" data-aos="fade-down">
           <img 
-          // src={leash} 
-          src="https://img.freepik.com/free-photo/shiba-inu-dog-taking-walk_23-2149478809.jpg?t=st=1773588804~exp=1773592404~hmac=c088ba7bb635433349e04ebe32b3c5da767d7db4d9e5b33e488fc97ef0c6b513&w=1480"
+          src={leash} 
+          // src="https://img.freepik.com/free-photo/shiba-inu-dog-taking-walk_23-2149478809.jpg?t=st=1773588804~exp=1773592404~hmac=c088ba7bb635433349e04ebe32b3c5da767d7db4d9e5b33e488fc97ef0c6b513&w=1480"
           className="h-60 w-full object-cover"  alt="colloar"/>
           <div className="p-5 text-center">
             <h3 className="font-semibold text-lg text-[#0B2F5B]">
@@ -58,8 +58,8 @@ const Section3 = () => {
         {/* Product 3 */}
         <div className="bg-white rounded-2xl shadow hover:shadow-xl transition overflow-hidden" data-aos="fade-up">
           <img 
-          // src={harness}
-          src="https://img.freepik.com/premium-psd/dog-leash-mockup-design_23-2150792294.jpg?w=1480"
+          src={harness}
+          // src="https://img.freepik.com/premium-psd/dog-leash-mockup-design_23-2150792294.jpg?w=1480"
            className="h-60 w-full object-cover"  alt="colloar"/>
           <div className="p-5 text-center">
             <h3 className="font-semibold text-lg text-[#0B2F5B]">
@@ -71,9 +71,9 @@ const Section3 = () => {
         {/* Product 4 */}
         <div className="bg-white rounded-2xl shadow hover:shadow-xl transition overflow-hidden" data-aos="fade-down">
           <img 
-          // src={accessory}
-          src="https://img.freepik.com/free-photo/top-view-pet-accessories_23-2150930409.jpg?t=st=1773588976~exp=1773592576~hmac=127a4a2217482e0fa406266d4fce63ccb6e6aff7b0364aa44ad54c3fae4fe7ad&w=1480"
-           className="h-60 w-full object-cover"  alt="colloar"/>
+          src={accessory}
+          // src="https://img.freepik.com/free-photo/top-view-pet-accessories_23-2150930409.jpg?t=st=1773588976~exp=1773592576~hmac=127a4a2217482e0fa406266d4fce63ccb6e6aff7b0364aa44ad54c3fae4fe7ad&w=1480"
+           className="h-60 w-full object-contain bg-[#ebf0e9]"  alt="colloar"/>
           <div className="p-5 text-center">
             <h3 className="font-semibold text-lg text-[#0B2F5B]">
               Pet Accessories
